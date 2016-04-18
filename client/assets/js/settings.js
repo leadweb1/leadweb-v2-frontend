c60030a67724d344f9c69d2ae814b68a9170483a
Christophe Faribault <christophefaribault@gmail.com>
2016-04-18 15:42:02 -0400

    Updated apiUrl

--- client/assets/js/settings.js
+++ client/assets/js/settings.js
@@ -12,6 +12,6 @@ var appConfig = {
   postUrl : 'http://astral-analytics.picard.lead-web.com/post-session',
   postKey : '_ujWA-rIA8IQfUD1',
   //driveUrl: 'https://spreadsheets.google.com/feeds/list/1I3ymWKzSZHpV4wicdLLpbmBjLaOhUp7aXncA3W9j_Ok/'
-  apiUrl  : 'http://127.0.0.1:8000',
+  apiUrl  : 'http://leadweb-v2.leadweb1.demo.lead-web.com',
   facebookAppId: '1601298056859713',
 };
